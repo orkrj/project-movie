bb3f22fd614a56ad4950f1806966b806ffcfd81f
zun <k5orkrj@gmail.com>
2025-02-03 23:59:20 +0900

    feat: add dependency and config for jacoco

--- api/src/test/java/hanghae/api/ApiApplicationTests.java
+++ api/src/test/java/hanghae/api/ApiApplicationTests.java
@@ -2,8 +2,10 @@ package hanghae.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ApiApplicationTests {
 
     @Test
